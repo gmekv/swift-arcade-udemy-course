8c448af8075ff9acd9cfd1fb6ba94b9dce665c62
Giorgi <g.mekvabishvili>
2026-01-13 17:44:45 +0400

    Initial Commit

---
+++ Bankey/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bankey
+//
+//  Created by Giorgi Mekvabishvili on 13.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BankeyTests/BankeyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BankeyTests.swift
+//  BankeyTests
+//
+//  Created by Giorgi Mekvabishvili on 13.01.26.
+//
+
+import Testing
+@testable import Bankey
+
+struct BankeyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
